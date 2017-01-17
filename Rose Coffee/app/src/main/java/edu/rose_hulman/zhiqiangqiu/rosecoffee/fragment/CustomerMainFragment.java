a212c845a264b1da2b4c3c25685fde3bdc070d4a
MainTankJerry <qiuz@rose-hulman.edu>
2017-01-16 20:25:00 -0500

    static tabbed fragments set is added successfully

--- Rose Coffee/app/src/main/java/edu/rose_hulman/zhiqiangqiu/rosecoffee/fragment/CustomerMainFragment.java
+++ Rose Coffee/app/src/main/java/edu/rose_hulman/zhiqiangqiu/rosecoffee/fragment/CustomerMainFragment.java
@@ -2,7 +2,11 @@ package edu.rose_hulman.zhiqiangqiu.rosecoffee.fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,17 +18,90 @@ import edu.rose_hulman.zhiqiangqiu.rosecoffee.R;
  */
 public class CustomerMainFragment extends Fragment {
 
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static int int_items = 3 ;
 
     public CustomerMainFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_main, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /**
+         *Inflate tab_layout and setup Views.
+         */
+        View x =  inflater.inflate(R.layout.tabbed_customer_main,null);
+        tabLayout = (TabLayout) x.findViewById(R.id.tabs);
+        viewPager = (ViewPager) x.findViewById(R.id.viewpager);
+
+        /**
+         *Set an Apater for the View Pager
+         */
+        viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+
+        /**
+         * Now , this is a workaround ,
+         * The setupWithViewPager dose't works without the runnable .
+         * Maybe a Support Library Bug .
+         */
+
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+            }
+        });
+
+        return x;
+
+    }
+
+    class MyAdapter extends FragmentPagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Return fragment with respect to Position .
+         */
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch (position){
+                case 0 : return new LocationAndTimeFragment();
+                case 1 : return new OrderDetailFragment();
+                case 2 : return new ConfirmAndCheckOutFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+
+            return int_items;
+
+        }
+
+        /**
+         * This method returns the title of the tab according to the position.
+         */
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position){
+                case 0 :
+                    return "Location and Time";
+                case 1 :
+                    return "Order Detail";
+                case 2 :
+                    return "Confirm and Check Out";
+            }
+            return null;
+        }
     }
 
 }
