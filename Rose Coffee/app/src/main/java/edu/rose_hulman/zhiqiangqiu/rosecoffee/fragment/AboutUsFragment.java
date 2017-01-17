847191133486034a18f2d1b59e1b8b3e5704bace
MainTankJerry <qiuz@rose-hulman.edu>
2017-01-16 19:36:41 -0500

    fix id issues

--- Rose Coffee/app/src/main/java/edu/rose_hulman/zhiqiangqiu/rosecoffee/fragment/AboutUsFragment.java
+++ Rose Coffee/app/src/main/java/edu/rose_hulman/zhiqiangqiu/rosecoffee/fragment/AboutUsFragment.java
@@ -1,9 +1,16 @@
 package edu.rose_hulman.zhiqiangqiu.rosecoffee.fragment;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 
 /**
  * Created by JerryQiu on 1/16/17.
  */
 public class AboutUsFragment extends Fragment {
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static int int_items = 3 ;
+
+    
 }
