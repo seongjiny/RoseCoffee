5bb3a6f52f17c70e61c41627bfc21bb652859c31
MainTankJerry <qiuz@rose-hulman.edu>
2017-02-06 11:59:15 -0500

    minor commit

--- Rose Coffee/app/src/main/java/edu/rose_hulman/zhiqiangqiu/rosecoffee/fragment/AccountInformationFragment.java
+++ Rose Coffee/app/src/main/java/edu/rose_hulman/zhiqiangqiu/rosecoffee/fragment/AccountInformationFragment.java
@@ -1,15 +1,12 @@
 package edu.rose_hulman.zhiqiangqiu.rosecoffee.fragment;
 
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
-import android.widget.Spinner;
 import android.widget.Switch;
 
 import edu.rose_hulman.zhiqiangqiu.rosecoffee.MainActivity;
@@ -28,7 +25,6 @@ public class AccountInformationFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -39,34 +35,10 @@ public class AccountInformationFragment extends Fragment {
         mIsDeliverySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // do something, the isChecked will be true if the switch is in the On position
-                if (isChecked) {
-                    mUser.setmIsCustomer(true);
-                } else {
-
-                }
+                mUser.setIsCustomer(!isChecked);
             }
         });
 
-
-
         return view;
     }
-
-    public final void showDialog(String name){
-        final Dialog mydialog = new Dialog(getActivity());
-        mydialog.setTitle("Edit "+name);
-        mydialog.setContentView(R.layout.dialog_edit_account_info);
-        Spinner nameSpinner = (Spinner) mydialog.findViewById(R.id.add_drink_name_spinner);
-        Spinner sizeSpinner = (Spinner) mydialog.findViewById(R.id.add_drink_size_spinner);
-        String[] drinkNames = new String[]{"Strawberry Frappuccino","Cappuccino","Cafe Latte"};
-        String[] sizeNames = new String[]{"Venti","Grande","Tall"};
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,drinkNames);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,sizeNames);
-        nameSpinner.setAdapter(adapter1);
-        sizeSpinner.setAdapter(adapter2);
-        mydialog.show();
-    }
-
-
-
 }
