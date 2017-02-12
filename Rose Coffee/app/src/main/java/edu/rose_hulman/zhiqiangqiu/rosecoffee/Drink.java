71c0014720b81e86614ffaab643652e79f4cdc3c
Jim Yoon <yoons1@rose-hulman.edu>
2017-02-12 17:54:42 -0500

    Adapter for order detail is done,.... its alot!

---
+++ Rose Coffee/app/src/main/java/edu/rose_hulman/zhiqiangqiu/rosecoffee/Drink.java
@@ -0,0 +1,39 @@
+package edu.rose_hulman.zhiqiangqiu.rosecoffee;
+
+/**
+ * Created by yoons1 on 2/12/2017.
+ */
+public class Drink implements MenuItem{
+    private String size;
+    private String name;
+    private String comment;
+    public Drink(String name,String size, String comment){
+        this.name=name;
+        this.size=size;
+        this.comment=comment;
+    }
+    @Override
+    public String getSize() {
+        return size;
+    }
+    @Override
+    public void setSize(String size) {
+        this.size = size;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String getComment() {
+        return comment;
+    }
+    @Override
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
--- Rose Coffee/app/src/main/java/edu/rose_hulman/zhiqiangqiu/rosecoffee/fragment/OrderDetailAdapter.java
+++ Rose Coffee/app/src/main/java/edu/rose_hulman/zhiqiangqiu/rosecoffee/fragment/OrderDetailAdapter.java
@@ -1,16 +1,26 @@
 package edu.rose_hulman.zhiqiangqiu.rosecoffee.fragment;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import edu.rose_hulman.zhiqiangqiu.rosecoffee.Order;
+import edu.rose_hulman.zhiqiangqiu.rosecoffee.Drink;
+import edu.rose_hulman.zhiqiangqiu.rosecoffee.MenuItem;
 import edu.rose_hulman.zhiqiangqiu.rosecoffee.R;
+import edu.rose_hulman.zhiqiangqiu.rosecoffee.Snack;
 
 /**
  * Created by yoons1 on 2/12/2017.
@@ -19,7 +29,8 @@ import edu.rose_hulman.zhiqiangqiu.rosecoffee.R;
 public class OrderDetailAdapter extends RecyclerView.Adapter<OrderDetailAdapter.ViewHolder>{
     private Context mContext;
     private RecyclerView mRecyclerView;
-    private ArrayList<Order> mOrders = new ArrayList<>();
+    private View OrderDetailView;
+    private ArrayList<MenuItem> mMenuItems = new ArrayList<>();
 
     public OrderDetailAdapter(Context context,RecyclerView recyclerView){
         mContext = context;
@@ -34,24 +45,158 @@ public class OrderDetailAdapter extends RecyclerView.Adapter<OrderDetailAdapter.
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Order order = mOrders.get(position);
-        holder.mOrderNameTextView.setText(order.getOrderID());
+        MenuItem menuItem = mMenuItems.get(position);
+        holder.mMenuNameTextView.setText(menuItem.getName());
+    }
+    public void removeMenuItem(int position){
+        MenuItem clearedItem = mMenuItems.get(position);
+        undo(clearedItem,position);
+        mMenuItems.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, mMenuItems.size());
     }
 
+    private void undo(final MenuItem clearedItem,final int position) {
+
+        Snackbar snackbar = Snackbar
+                .make(mRecyclerView,"Item Removed",Snackbar.LENGTH_LONG)
+                .setAction("UNDO",new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        addMenuItem(position,clearedItem);
+                    }
+                });
+        snackbar.show();
+    }
+
+    public void editMenuItem(int position, MenuItem menuItem){
+        MenuItem clearedItem = mMenuItems.get(position);
+        mMenuItems.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, mMenuItems.size());
+        addMenuItem(position,menuItem);
+    }
+    public void addMenuItem(MenuItem menuItem){
+        mMenuItems.add(0, menuItem);
+        notifyDataSetChanged();
+    }
+    public void addMenuItem(int position,MenuItem menuItem){
+        mMenuItems.add(position, menuItem);
+        notifyDataSetChanged();
+    }
     @Override
     public int getItemCount() {
-        return mOrders.size();
+        return mMenuItems.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener{
-        private TextView mOrderNameTextView;
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener,View.OnClickListener{
+        private TextView mMenuNameTextView;
         public ViewHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mMenuItems.get(getAdapterPosition()) instanceof Drink){
+                        editDrink(getAdapterPosition());
+                    }else{
+                        editSnack(getAdapterPosition());
+                    }
+                }
+            });
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    removeMenuItem(getAdapterPosition());
+                    return false;
+                }
+            });
+            mMenuNameTextView = (TextView) itemView.findViewById(R.id.menu_item_text_view);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if(mMenuItems.get(getAdapterPosition()) instanceof Drink){
+                editDrink(getAdapterPosition());
+            }else{
+                editSnack(getAdapterPosition());
+            }
         }
 
         @Override
         public boolean onLongClick(View v) {
+            removeMenuItem(getAdapterPosition());
             return false;
         }
+    }
+    public void editDrink(final int position){
+        MenuItem currentItem = mMenuItems.get(position);
+        final Dialog mydialog = new Dialog(mContext);
+        mydialog.setContentView(R.layout.dialog_add_drink);
+        final Spinner nameSpinner = (Spinner) mydialog.findViewById(R.id.add_drink_name_spinner);
+        final Spinner sizeSpinner = (Spinner) mydialog.findViewById(R.id.add_drink_size_spinner);
+        String[] drinkNames = new String[]{"Strawberry Frappuccino","Cappuccino","Cafe Latte"};
+        String[] sizeNames = new String[]{"Venti","Grande","Tall"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(mContext,android.R.layout.simple_spinner_item,drinkNames);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(mContext,android.R.layout.simple_spinner_item,sizeNames);
+
+        nameSpinner.setAdapter(adapter1);
+        sizeSpinner.setAdapter(adapter2);
+        int drinkIndex = Arrays.asList(drinkNames).indexOf(currentItem.getName());
+        nameSpinner.setSelection(drinkIndex);
+        int sizeIndex = Arrays.asList(sizeNames).indexOf(currentItem.getSize());
+        sizeSpinner.setSelection(sizeIndex);
+        final EditText editComment = (EditText)mydialog.findViewById(R.id.add_drink_edit_text);
+        editComment.setText(currentItem.getComment());
+
+
+        Button buttonOK = (Button) mydialog.findViewById(R.id.add_drink_ok);
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drink drink = new Drink(
+                        nameSpinner.getSelectedItem().toString(),
+                        sizeSpinner.getSelectedItem().toString(),
+                        editComment.getText()+"");
+                editMenuItem(position,drink);
+                mydialog.dismiss();
+            }
+        });
+        Button buttonCancel = (Button) mydialog.findViewById(R.id.add_drink_cancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mydialog.dismiss();
+            }
+        });
+        mydialog.show();
+    }
+    public void editSnack(final int position){
+        final Dialog mydialog = new Dialog(mContext);
+        mydialog.setContentView(R.layout.dialog_add_snack);
+        final Spinner nameSpinner = (Spinner) mydialog.findViewById(R.id.add_snack_name_spinner);
+        String[] snackNames = new String[]{"Muffin","Apple Sauce","Chips"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(mContext,android.R.layout.simple_spinner_item,snackNames);
+        nameSpinner.setAdapter(adapter1);
+        int snackIndex= Arrays.asList(snackNames).indexOf(mMenuItems.get(position).getName());
+        nameSpinner.setSelection(snackIndex);
+
+        Button buttonOK = (Button) mydialog.findViewById(R.id.add_snack_ok);
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snack snack = new Snack(nameSpinner.getSelectedItem().toString());
+                editMenuItem(position,snack);
+                mydialog.dismiss();
+            }
+        });
+        Button buttonCancel = (Button) mydialog.findViewById(R.id.add_snack_cancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("d","cancel");
+                mydialog.dismiss();
+            }
+        });
+        mydialog.show();
     }
 }
